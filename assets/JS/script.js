260de424ed6251192d6ff7d3c46d59cc905a0daf
Katherinna <k.candia@duocuc.cl>
2025-09-11 08:46:46 -0300

    js listo, conexión men´´´´ú tambien, solo falta arreglar problema blog

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -476,3 +476,72 @@ function aplicarDescuento() {
 
 // Ejecutar al cargar carrito
 document.addEventListener("DOMContentLoaded", mostrarCarrito);
+
+
+//Contacto
+//validar correo
+function validarCorreo(correo) {
+    const regex = /^[\w.+-]+@(duoc\.cl|profesor\.duoc\.cl|gmail\.com)$/i;
+    return regex.test(correo);
+}
+
+//boton
+document.addEventListener("DOMContentLoaded" , () => {
+    const formulario = document.querySelector(".formulario_contacto"); //chatgpt me digo que le pusiera esto, pero la profe no lo hizo asiiii, será porque usé bootstrap?
+    const nombreInput = document.getElementById("nombre");
+    const correoInput = document.getElementById("correo");
+    const mensajeInput = document.getElementById("mensaje");
+    const mensajeAlert = document.getElementById("mensajeEnv"); //pongo esto solo porque no me funciona nadaaa
+
+
+
+    //limpiar mensaje al ingresar datos en el imput
+    [nombreInput, correoInput, mensajeInput].forEach(input => {
+        input.addEventListener ("input", () => {
+            input.setCustomValidity ("");
+            mensajeAlert.innerText= "";
+        });
+    });
+
+    //conecta el formulario con todo esto
+    formulario.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        //limpiar mensaje sprevios
+
+        mensajeAlert.innerText = "";
+
+        //validar que no esten vacíos
+        //nombre
+
+        if(!nombreInput.value.trim() || !correoInput.value.trim() || !mensajeInput.value.trim()) {
+            alert("Rellene todos los campos :)");
+            return;
+        }
+
+        //validar correo
+        if(!validarCorreo(correoInput.value.trim())){
+            alert("El correo debe ser '@duoc.cl', '@profesor.duoc.cl', '@gmail.com'.");
+            return;
+        }
+
+        mostrarMensajePushup();
+
+        // mensaje "todo bien"
+        mensajeAlert.innerText = "Mensaje enviado correctamente. Lo contactaremos a la brevedad."; //no me convence el mensaje xd
+        formulario.reset();
+    
+    });
+    
+    //perplexity, push up bonito
+    function mostrarMensajePushup() {
+        const mensaje = document.getElementById("mensajePushup");
+        mensaje.style.display = "block";
+        setTimeout(() => {
+            mensaje.style.display = "none";
+        }, 3000); // desaparece después de 3 segundos
+}
+
+});
+
+
