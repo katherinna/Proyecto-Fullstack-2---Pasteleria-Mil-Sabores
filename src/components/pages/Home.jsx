b8728dc72dc7bf32aae388572ffc82a10942e2d8
PAAWSA02LC1323 <Duoc@PAAWSA02LC1323.duoc.acad>
2025-10-23 18:53:16 -0300

    avances main

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Home = () => {
+    return (
+        <main>          
+            {/* Enlace que usa React Router pero carga el formulario estático */}
+            <Link to="/registro_usuario"></Link>
+        </main>
+    )
+};
+export default Home;
